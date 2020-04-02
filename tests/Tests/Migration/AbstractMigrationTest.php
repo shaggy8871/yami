db77cea505f1cdf08748c389234ed8e9846a45d6
John Ginsberg <john.ginsberg@gmail.com>
2020-04-02 20:33:23 +0100

    Improved migration test, defaulting to dry-run

--- tests/Tests/Migration/AbstractMigrationTest.php
+++ tests/Tests/Migration/AbstractMigrationTest.php
@@ -13,6 +13,8 @@ class AbstractMigrationTest extends TestCase
 
     public function testRootNodeMigration(): void
     {
+        $args = new Args([]);
+
         // Seed the config
         Bootstrap::seedConfig([
             'environments' => [
@@ -22,14 +24,23 @@ class AbstractMigrationTest extends TestCase
                 ],
             ]
         ]);
+        Bootstrap::createMockYaml($args);
 
-        $testMigration = include_once('./tests/migrations/0000000000_test_class.php');
-        $migrationInstance = new \TestClass(Migrate::ACTION, (object) ['className' => 'TestClass', 'filePath' => './tests/migrations/0000000000_test_class.php', 'uniqueId' => '0000000000_test_class',], new Args([]));
+        $migration = (object) [
+            'filePath' => './tests/migrations/0000000000_test_class.php',
+            'uniqueId' => '0000000000_test_class'
+        ];
+
+        include_once($migration->filePath);
+
+        $migrationInstance = new \TestClass(Migrate::ACTION, $migration, $args);
 
         $rootNode = $migrationInstance->get('.');
 
         $this->assertInstanceOf(Node::class, $rootNode);
-        $this->assertEquals($migrationInstance->get('.'), new Node(['foo' => 'baz'], '.'));
+        $this->assertEquals($rootNode, new Node(['foo' => 'baz'], '.'));
+
+        Bootstrap::deleteMockYaml($args);
     }
 
 }
